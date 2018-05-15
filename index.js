15b1f715eb4a295e0a277471d649460f52bd29e5
amrudees@yahoo.com <amrudees@yahoo.com>
2018-05-15 01:21:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ and returns the first element that matches. */
 function getFirstSelector(selector){
   return document.querySelector(selector)
 }
+
 /*Define a function `nestedTarget()` that pulls a `.target` out of `#nested`
   (`#` is used for IDs in selectors — but you knew that because you[read the docs]
   (https://developer.mozilla.org/en-US/docs/Web/Guide/CSS/Getting_Started/Selectors), right? :) ).
   (Note that in `index.html` `#nested` and `.target` just _happen_ to be `div`s.
   This method should work with arbitrary elements.)*/
 function nestedTarget(selector){
-  const lis = document.querySelectorAll(selector)
+  return document.getElementById('nested').querySelectorAll(selector)
 
 }
 
