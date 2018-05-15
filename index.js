0b6ccbe274ea2222c5cc82276938a5ef02b31fa0
amrudees@yahoo.com <amrudees@yahoo.com>
2018-05-15 01:02:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-/*Define a function `getFirstSelector(selector)`, which accepts a selector 
+/*Define a function `getFirstSelector(selector)`, which accepts a selector
 and returns the first element that matches. */
-
-/*Define a function `nestedTarget()` that pulls a `.target` out of `#nested` 
-  (`#` is used for IDs in selectors — but you knew that because you 
+function getFirstSelector(selector){
+  return 
+}
+/*Define a function `nestedTarget()` that pulls a `.target` out of `#nested`
+  (`#` is used for IDs in selectors — but you knew that because you
   [read the docs](https://developer.mozilla.org/en-US/docs/Web/Guide/CSS/Getting_Started/Selectors), right? :) ).
-  (Note that in `index.html` `#nested` and `.target` just _happen_ to be `div`s. 
+  (Note that in `index.html` `#nested` and `.target` just _happen_ to be `div`s.
   This method should work with arbitrary elements.)*/
-  
-/* Define a function `increaseRankBy(n)` that increases the ranks in all of the 
+
+/* Define a function `increaseRankBy(n)` that increases the ranks in all of the
   `.ranked-list`s by `n`. (You might need to make use of [`parseInt()`]
   (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt) */
   
