ef87567a7e796a60976d211d306bcb49d38ed1ff
amrudees@yahoo.com <amrudees@yahoo.com>
2018-05-15 02:45:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ function increaseRankBy(n){
   implement correctly, but not beyond your ability!)
   */
 function deepestChild(){
-  return document.getElementById('grand-node').getElementsByTagName('div').lastChild.textContent
-
+  l = document.getElementById('grand-node').getElementsByTagName('div').lastChild
+  return l.textContent
   }
